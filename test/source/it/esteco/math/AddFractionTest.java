92e66498eb820ad90ad8a5d55e330eb70a24f33b
dcampagna <campagna@esteco.com>
2016-10-17 16:41:37 +0200

    We now add negative, non-zero, zero integers

--- test/source/it/esteco/math/AddFractionTest.java
+++ test/source/it/esteco/math/AddFractionTest.java
@@ -26,8 +26,26 @@ public class AddFractionTest {
     }
 
     @Test
-    public void positiveNonZeroPlusPositiveNonZero() throws Exception {
+    public void nonNegativeNonZeroOperands() throws Exception {
         Fraction sum = new Fraction(4).plus(new Fraction(9));
         assertEquals(13, sum.intValue());
+    }
+
+    @Test
+    public void negativePlusPositiveNonZero() throws Exception {
+        Fraction sum = new Fraction(-2).plus(new Fraction(3));
+        assertEquals(1, sum.intValue());
+    }
+
+    @Test
+    public void positiveNonZeroPlusNegative() throws Exception {
+        Fraction sum = new Fraction(5).plus(new Fraction(-1));
+        assertEquals(4, sum.intValue());
+    }
+
+    @Test
+    public void negativeInputsAndNegativeOutputs() throws Exception {
+        Fraction sum = new Fraction(-6).plus(new Fraction(-8));
+        assertEquals(-14, sum.intValue());
     }
 }
