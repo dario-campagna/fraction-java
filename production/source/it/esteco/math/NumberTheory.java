69329465e973f24243b1e886d20dc4fe0e07ff5c
dcampagna <campagna@esteco.com>
2016-10-18 11:38:15 +0200

    Extracted gcd method to NumberTheory class

---
+++ production/source/it/esteco/math/NumberTheory.java
@@ -0,0 +1,14 @@
+package it.esteco.math;
+
+public class NumberTheory {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return Math.abs(a);
+    }
+
+}
--- test/source/it/esteco/math/GreatestCommonDivisorTest.java
+++ test/source/it/esteco/math/GreatestCommonDivisorTest.java
@@ -8,39 +8,31 @@ public class GreatestCommonDivisorTest {
 
     @Test
     public void sameArguments() throws Exception {
-        assertEquals(1, gcd(1,1));
-        assertEquals(2, gcd(2,2));
+        assertEquals(1, NumberTheory.gcd(1,1));
+        assertEquals(2, NumberTheory.gcd(2,2));
     }
 
     @Test
     public void relativelyPrime() throws Exception {
-        assertEquals(1, gcd(7, 9));
+        assertEquals(1, NumberTheory.gcd(7, 9));
     }
 
     @Test
     public void oneMultipleOfTheOther() throws Exception {
-        assertEquals(2, gcd(2, 4));
+        assertEquals(2, NumberTheory.gcd(2, 4));
     }
 
     @Test
     public void commonFactor() throws Exception {
-        assertEquals(2, gcd(18, 8));
+        assertEquals(2, NumberTheory.gcd(18, 8));
     }
 
     @Test
     public void negativeIntegers() throws Exception {
         // We like always positive gcd
-        assertEquals(4, gcd(-4, 8));
-        assertEquals(3, gcd(9, -30));
-        assertEquals(2, gcd(-6, -20));
+        assertEquals(4, NumberTheory.gcd(-4, 8));
+        assertEquals(3, NumberTheory.gcd(9, -30));
+        assertEquals(2, NumberTheory.gcd(-6, -20));
     }
 
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        return Math.abs(a);
-    }
 }
