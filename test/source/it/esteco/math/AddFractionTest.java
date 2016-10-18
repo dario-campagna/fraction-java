6aeecc71f0e7bf8ec333a8c1893333ae8ff94a4f
dcampagna <campagna@esteco.com>
2016-10-18 09:14:29 +0200

    Add fraction tests now use Fraction equals()

--- test/source/it/esteco/math/AddFractionTest.java
+++ test/source/it/esteco/math/AddFractionTest.java
@@ -9,50 +9,46 @@ public class AddFractionTest {
 
     @Test
     public void zeroPlusZero() throws Exception {
-        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() throws Exception {
-        assertEquals(7, new Fraction(7).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(7), new Fraction(7).plus(new Fraction(0)));
     }
 
     @Test
     public void zeroPlusNonZero() throws Exception {
-        assertEquals(11, new Fraction(0).plus(new Fraction(11)).intValue());
+        assertEquals(new Fraction(11), new Fraction(0).plus(new Fraction(11)));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() throws Exception {
-        assertEquals(13, new Fraction(4).plus(new Fraction(9)).intValue());
+        assertEquals(new Fraction(13), new Fraction(4).plus(new Fraction(9)));
     }
 
     @Test
     public void negativePlusPositiveNonZero() throws Exception {
-        assertEquals(1, new Fraction(-2).plus(new Fraction(3)).intValue());
+        assertEquals(new Fraction(1), new Fraction(-2).plus(new Fraction(3)));
     }
 
     @Test
     public void positiveNonZeroPlusNegative() throws Exception {
-        assertEquals(4, new Fraction(5).plus(new Fraction(-1)).intValue());
+        assertEquals(new Fraction(4), new Fraction(5).plus(new Fraction(-1)));
     }
 
     @Test
     public void negativeInputsAndNegativeOutputs() throws Exception {
-        assertEquals(-14, new Fraction(-6).plus(new Fraction(-8)).intValue());
+        assertEquals(new Fraction(-14), new Fraction(-6).plus(new Fraction(-8)));
     }
 
     @Test
     public void sameDenominators() throws Exception {
-        Fraction sum = new Fraction(1, 3).plus(new Fraction(4, 3));
-        assertEquals(5, sum.getNumerator());
-        assertEquals(3, sum.getDenominator());
+        assertEquals(new Fraction(5, 3), new Fraction(1, 3).plus(new Fraction(4, 3)));
     }
 
     @Test
     public void differentDenominators() throws Exception {
-        Fraction sum = new Fraction(1, 3).plus(new Fraction(1, 4));
-        assertEquals(7, sum.getNumerator());
-        assertEquals(12, sum.getDenominator());
+        assertEquals(new Fraction(7, 12), new Fraction(1, 3).plus(new Fraction(1, 4)));
     }
 }
