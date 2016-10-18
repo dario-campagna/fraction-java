d5f0f83b2f49f65152f42e5b6a3678c92088785d
dcampagna <campagna@esteco.com>
2016-10-18 10:02:48 +0200

    Our gcd method computes always positive GCD

--- test/source/it/esteco/math/GreatestCommonDivisorTest.java
+++ test/source/it/esteco/math/GreatestCommonDivisorTest.java
@@ -27,12 +27,20 @@ public class GreatestCommonDivisorTest {
         assertEquals(2, gcd(18, 8));
     }
 
+    @Test
+    public void negativeIntegers() throws Exception {
+        // We like always positive gcd
+        assertEquals(4, gcd(-4, 8));
+        assertEquals(3, gcd(9, -30));
+        assertEquals(2, gcd(-6, -20));
+    }
+
     private int gcd(int a, int b) {
         while (b != 0) {
             int t = b;
             b = a % b;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
